47f89f6bf4a424c4db23f9e88fc6f5884cf005f3
Benny Thadikaran <tbenny05@yahoo.com>
2025-05-10 17:19:15 +0530

    - Added json parameter to post and delete methods. - Added data parameter to patch and delete methods. - Add reasonable defaults for retry decorator. - Rewrite docstrings for retry decorator. - add a PATCH method. - Use a list of Throttlers to apply multiple rate limits. - Use `logging.warning` instead of deprecated `logging.warn` method. - Use a logger instance set to the module name.

--- aio_trader/AsyncRequest.py
+++ aio_trader/AsyncRequest.py
@@ -1,23 +1,33 @@
-from typing import Any, Dict, Optional, Any
+import asyncio
+import json
+import pathlib
+from typing import Any, Dict, List, Optional
+
+import aiohttp
 from throttler import Throttler
-import asyncio, aiohttp, logging, json, pathlib
+
+from .utils import configure_default_logger
 
 
-def retry(max_retries=50, base_wait=2, max_wait=60):
+def retry(max_retries=5, base_wait=3, max_wait=30):
     """
     Decorator that retries a function or method with exponential backoff
     in case of exceptions.
 
-    Parameters:
-    - max_retries (int): The maximum number of retry attempts.
-    - base_wait (float): The initial delay in seconds before the first retry.
-    - max_wait (float): The maximum delay in seconds between retries.
+    :param max_retries: The maximum number of retry attempts.
+    :type max_retries: int
+    :param base_wait: The initial delay in seconds before the first retry.
+    :type base_wait: float
+    :param max_wait: The maximum delay in seconds between retries.
+    :type max_wait: float
 
-    Usage:
-    @retry(max_retries=5, base_wait=2, max_wait=60)
-    async def your_function_or_method(*args, **kwargs):
-        # Your function or method logic goes here
-        pass
+    .. code::python
+
+        @retry(max_retries=5, base_wait=2, max_wait=60)
+        async def your_function_or_method(*args, **kwargs):
+            # Your function or method logic goes here
+            pass
+
     """
 
     def decorator(method):
@@ -29,12 +39,13 @@ def retry(max_retries=50, base_wait=2, max_wait=60):
                 try:
                     return await method(instance, *args, **kwargs)
                 except (
-                    aiohttp.ClientError,
-                    asyncio.TimeoutError,
+                    aiohttp.ClientConnectionError,
                     json.JSONDecodeError,
                     ConnectionError,
                 ) as e:
-                    instance.logger.warn(f"Attempt {retries + 1} failed: {e}")
+                    instance.logger.warning(
+                        f"Attempt {retries + 1} failed: {e}"
+                    )
 
                     # Calculate the wait time using exponential backoff
                     wait = min(base_wait * (2**retries), max_wait)
@@ -44,9 +55,8 @@ def retry(max_retries=50, base_wait=2, max_wait=60):
 
                     retries += 1
                 except RuntimeError as e:
-                    instance.logger.warn(str(e))
                     await instance.close_session()
-                    raise e
+                    return instance.logger.exception(f"An error occurred {e}")
 
             instance.logger.warn("Exceeded maximum retry attempts. Exiting.")
 
@@ -59,59 +69,60 @@ class AsyncRequest:
     """
     A wrapper class for making async requests using aiohttp
 
-    Methods can raise
-        - RuntimeError
-        - ConnectionError
-        - asyncio.TimeoutError
+    :param throttlers: Default list of throttlers to apply to all requests.
+    :type throttlers: List[throttler.Throttler]
+    :param kwargs: Additional keyword arguments passed to aiohttp.ClientSession
 
-    :param logger: A logging.Logger instance
-    :type logger: logging.Logger
-    :param throttle: Default throttler for all requests.
-    :type throttle: throttler.Throttler
-    :param **kwargs: Additional keyword arguments passed to aiohttp.ClientSession
     """
 
     session: aiohttp.ClientSession
-    throttle: Throttler
 
     def __init__(
         self,
-        logger: logging.Logger,
-        throttle: Optional[Throttler] = None,
+        throttlers: List[Throttler],
         cookie_path: Optional[pathlib.Path] = None,
         **kwargs,
     ) -> None:
         self.session_args: Dict[str, Any] = kwargs
-        self.logger = logger
+        self.logger = configure_default_logger(__name__)
         self.cookie_path = cookie_path
 
-        if throttle is None:
-            raise RuntimeError("Default Throttler is required")
-
-        self.throttle = throttle
+        self.throttlers = throttlers
 
     async def __aenter__(self):
+        """On entering async context manager"""
         self.start_session()
         return self
 
     async def __aexit__(self, *_):
+        """On exiting async context manager"""
         await self.close_session()
 
     def start_session(self):
+        """Starts the aiohttp.ClientSession and maps the HTTP methods"""
         self.session = aiohttp.ClientSession(**self.session_args)
         self.fn_map = {
             "GET": self.session.get,
             "POST": self.session.post,
             "PUT": self.session.put,
             "DELETE": self.session.delete,
+            "PATCH": self.session.patch,
         }
 
     async def close_session(self):
+        """Close the session"""
         if not self.session.closed:
             await self.session.close()
 
     @retry(max_retries=5, base_wait=3, max_wait=30)
     async def __req(self, method, endpoint, **kwargs):
+        """
+        Make an http request using the specified HTTP method.
+
+        :raise RuntimeError: if HTTP response code is 400, 403, 429
+        :raise ConnectionError: for any other HTTP response error code
+
+        """
         async with self.fn_map[method](endpoint, **kwargs) as response:
             if response.ok:
                 self.cookies = response.cookies
@@ -148,94 +159,148 @@ class AsyncRequest:
         endpoint,
         params=None,
         headers=None,
-        throttle: Optional[Throttler] = None,
+        throttlers: Optional[List[Throttler]] = None,
     ) -> Any:
         """
         GET Request
 
         :param endpoint: url endpoint
-        :type str
+        :type endpoint: str
         :param params: Query string params
-        :type params: dict
+        :type params: dict or any iterable
         :param headers: Additional http headers
-        :type dict
-        :param throttle: Overide the default throttler
-        :type throttle.Throttler
+        :type headers: dict
+        :param throttlers: Default None. Overide the default throttlers
+        :type throttlers: Optional[List[Throttler]]
+
         """
+        t = throttlers or self.throttlers
 
-        t = throttle if throttle else self.throttle
+        await self.__apply_throttle(t)
 
-        async with t:
-            return await self.__req(
-                "GET", endpoint, params=params, headers=headers
-            )
+        return await self.__req("GET", endpoint, params=params, headers=headers)
 
     async def post(
         self,
         endpoint,
         data=None,
         params=None,
+        json=None,
         headers=None,
-        throttle: Optional[Throttler] = None,
+        throttlers: Optional[List[Throttler]] = None,
     ) -> Any:
         """
         POST Request
 
         :param endpoint: url endpoint
-        :type str
+        :type endpoint: str
         :param data: data to send in body of POST request
-        :type params: aiohttp.FormData
+        :type data: aiohttp.FormData or any object that can be passed to formdata
+        :param params: data to send in body of POST request
+        :type params: dict or any iterable
+        :param json: Any json compatible python object (optional). json and
+         data parameters could not be used at the same time.
+        :type json: dict
         :param headers: Additional http headers
-        :type dict
-        :param throttle: Overide the default throttler
-        :type throttle.Throttler
+        :type headers: dict
+        :param throttlers: Default None. Overide the default throttlers
+        :type throttlers: Optional[List[Throttler]]
         """
-        t = throttle if throttle else self.throttle
+        t = throttlers or self.throttlers
 
-        async with t:
-            return await self.__req(
-                "POST", endpoint, data=data, params=params, headers=headers
-            )
+        await self.__apply_throttle(t)
+
+        return await self.__req(
+            "POST",
+            endpoint,
+            data=data,
+            params=params,
+            json=json,
+            headers=headers,
+        )
 
     async def put(
         self,
         endpoint,
         data=None,
         headers=None,
-        throttle: Optional[Throttler] = None,
+        throttlers: Optional[List[Throttler]] = None,
     ) -> Any:
         """PUT Request
 
         :param endpoint: url endpoint
-        :type str
+        :type endpoint: str
         :param data: data to send in body of PUT request
-        :type params: aiohttp.FormData
+        :type data: aiohttp.FormData or any object that can be passed to formdata
         :param headers: Additional http headers
-        :type dict
-        :param throttle: Overide the default throttler
-        :type throttle.Throttler
+        :type headers: dict
+        :param throttlers: Default None. Overide the default throttlers
+        :type throttlers: Optional[List[Throttler]]
         """
-        t = throttle if throttle else self.throttle
+        t = throttlers or self.throttlers
 
-        async with t:
-            return await self.__req("PUT", endpoint, data=data, headers=headers)
+        await self.__apply_throttle(t)
+
+        return await self.__req("PUT", endpoint, data=data, headers=headers)
 
     async def delete(
         self,
         endpoint,
+        data=None,
+        json=None,
         headers=None,
-        throttle: Optional[Throttler] = None,
+        throttlers: Optional[List[Throttler]] = None,
     ) -> Any:
-        """DELETE Request
+        """
+        DELETE Request
 
         :param endpoint: url endpoint
-        :type str
+        :type endpoint: str
+        :param data: data to send in body of POST request
+        :type data: aiohttp.FormData or any object that can be passed to formdata
+        :param json: Any json compatible python object (optional). json and
+         data parameters could not be used at the same time.
+        :type json: dict
         :param headers: Additional http headers
-        :type dict
-        :param throttle: Overide the default throttler
-        :type throttle.Throttler
+        :type headers: dict
+        :param throttlers: Default None. Overide the default throttlers
+        :type throttlers: Optional[List[Throttler]]
         """
-        t = throttle if throttle else self.throttle
+        t = throttlers or self.throttlers
 
-        async with t:
-            return await self.__req("DELETE", endpoint, headers=headers)
+        await self.__apply_throttle(t)
+
+        return await self.__req(
+            "DELETE", endpoint, headers=headers, data=data, json=json
+        )
+
+    async def patch(
+        self,
+        endpoint,
+        data=None,
+        headers=None,
+        throttlers: Optional[List[Throttler]] = None,
+    ) -> Any:
+        """
+        DELETE Request
+
+        :param endpoint: url endpoint
+        :type endpoint: str
+        :param data: data to send in body of POST request
+        :type data: aiohttp.FormData or any object that can be passed to formdata
+        :param headers: Additional http headers
+        :type headers: dict
+        :param throttlers: Default None. Overide the default throttlers
+        :type throttlers: Optional[List[Throttler]]
+        """
+        t = throttlers or self.throttlers
+
+        await self.__apply_throttle(t)
+
+        return await self.__req("PATCH", endpoint, headers=headers, data=data)
+
+    @staticmethod
+    async def __apply_throttle(throttlers: List[Throttler]):
+        for t in throttlers:
+            async with t:
+                pass
